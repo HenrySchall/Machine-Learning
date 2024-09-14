7910d66d59ceb1ea15943dd50aef12817f69e255
Henrique Werner <henrique.schall@outlook.com>
2024-09-14 14:36:03 -0300

    .

--- 1.Introdução.py
+++ 1.Introdução.py
@@ -3,19 +3,19 @@ import random as random
 import radian as rd
 import pandas as pd
 import numpy as np
-from scipy import stats
+import scipy.stats as stats
+import datetime  
 import seaborn as sns
 import seaborn.objects as so
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+import statsmodels.tsa.stattools as sm
 import plotly.express as px
-import yfinance as yf
 import math as math
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.seasonal import seasonal_decompose as decompose
+import statsmodels.api as sm
 
 ##################
 ### Introdução ###
